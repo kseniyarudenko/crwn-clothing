4c887dda6acf2b311e37eb30a2fcd666345ef2d8
Kseniya <kseniya.rudenko@firstml.co.nz>
2023-01-19 16:12:48 +1300

    Add styling to categories

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
@@ -24,10 +26,10 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
           <div className="category-body-container">
-            <div className="background-image" />
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
