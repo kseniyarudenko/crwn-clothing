5d4a8563792d490a23fd7030391743c2f23910bb
Kseniya <kseniya.rudenko@firstml.co.nz>
2023-03-23 13:22:07 +1300

    Update links from sign-in to auth
    Update route from sign-in to auth

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/sign-in" element={<Authentification />} />
+        <Route path="/auth" element={<Authentification />} />
       </Route>
     </Routes>
   );
