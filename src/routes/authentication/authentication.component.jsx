e8fd3b190ccc7de6e2ce4408be382f78fbb8d61e
Kseniya <kseniya.rudenko@firstml.co.nz>
2023-03-23 14:20:03 +1300

    Update Authentification page with SignInForm

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,22 +1,11 @@
-import {
-  createUserDocumentFromAuth,
-  signInWithGooglePopup,
-} from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import Button from "../../components/button/button.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import "./authentication.styles.scss";
 
 const Authentification = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <Button buttonType="google" onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </Button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
